12c09ca3c59c69b45dc3b79882f37108064b62a0
Drwby <kevin_b@cmu.ac.th>
2025-10-09 01:59:54 +0700

    Lab09 DONE

--- src/views/event/EventFormView.vue
+++ src/views/event/EventFormView.vue
@@ -1,49 +1,70 @@
 <script setup lang="ts">
-import type { Event as Event } from '@/types'
-import { ref } from 'vue'
-import EventService from '@/services/EventService'
+import type { Event, Organizer } from '@/types'
+import BaseInput from '@/components/BaseInput.vue'
+import BaseSelect from '@/components/BaseSelect.vue'
+import { ref, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
+import EventService from '@/services/EventService'
 import { useMessageStore } from '@/stores/message'
+import OrganizerService from '@/services/OrganizerService' 
+
 const event = ref<Event>({
-    id:null,
-    category: '',
-    title: '',
-    description: '',
-    location: '',
-    date: '',
-    time: '',
-    petsAllowed: false,
-    organizer: '',
+  id: null,
+  category: '',
+  title: '',
+  description: '',
+  location: '',
+  date: '',
+  time: '',
+  petsAllowed: false,
+  organizer: {
+    id: 0,
+    name: ''
+  }
 })
+
 const router = useRouter()
 const store = useMessageStore()
+
 function saveEvent() {
-    EventService.saveEvent(event.value).then((response) => {
-        router.push({ name: 'event-detail-view', params: { id: response.data.id } })
-        store.updateMessage('You are successfully add a new event for ' + response.data.title)
-        setTimeout(() => {
-            store.resetMessage()
-         }, 3000)
-    }).catch(() => {
-router.push({ name: 'network-error-view' })
-})
+  EventService.saveEvent(event.value)
+    .then((response) => {
+      router.push({ name: 'event-detail-view', params: { id: response.data.id } })
+      store.updateMessage('You have successfully added a new event: ' + response.data.title)
+      setTimeout(() => store.resetMessage(), 3000)
+    })
+    .catch(() => {
+      router.push({ name: 'network-error-view' })
+    })
 }
+
+const organizers = ref<Organizer[]>([])
+onMounted(() => {
+  OrganizerService.getOrganizers()
+    .then((response) => {
+      organizers.value = response.data
+    })
+    .catch(() => {
+      router.push({ name: 'network-error-view' })
+    })
+})
 </script>
+
 
 <template>
   <div>
     <h1>Create an event</h1>
     <form @submit.prevent="saveEvent">
-      <label>Category</label>
-      <input v-model="event.category" type="text" placeholder="Category" class="field" />
+      <BaseInput v-model="event.category" type="text" label="Category" />
       <h3>Name & describe your event</h3>
-      <label>Title</label>
-      <input v-model="event.title" type="text" placeholder="Title" class="field" />
-      <label>Description</label>
-      <input v-model="event.description" type="text" placeholder="Description" class="field" />
+      <BaseInput v-model="event.title" type="text" label="Title" />
+      <BaseInput v-model="event.description" type="text" label="Description" />
       <h3>Where is your event?</h3>
-      <label>Location</label>
-      <input v-model="event.location" type="text" placeholder="Location" class="field" />
+      <BaseInput v-model="event.location" type="text" label="Location" />
+      <h3>Who is your organizer?</h3>
+      <label>Select an Organizer</label>
+      <BaseSelect v-model="event.organizer.id" :options="organizers" />
+
       <button class="button" type="submit">Submit</button>
     </form>
 
